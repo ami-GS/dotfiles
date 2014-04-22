5c58435390d098794579fba17575d27820f179e3
Daiki Aminaka <1991.daiki@gmail.com>
2014-04-22 14:20:57 +0900

    added highlight symbol

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,6 +18,13 @@
 
 (add-to-list 'load-path "~/.emacs.d/auto-complete")
 
+(require 'highlight-symbol)
+(setq highlight-symbol-colors '("RoyalBlue1" "SpringGreen1" "DeepPink1" "OliveDrab"))
+(global-set-key (kbd "<f3>") 'highlight-symbol-at-point)
+(global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
+
+(require 'auto-highlight-symbol)
+(global-auto-highlight-symbol-mode t)
 
 ;;;; auto-complete
 (require 'auto-complete-config)
