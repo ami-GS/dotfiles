915347908db9762cb9dabf1389a0a58f3ff5e7c4
Ubuntu <ubuntu@ip-172-31-6-42.ap-northeast-1.compute.internal>
2016-04-17 06:18:35 +0000

    add setup shell script

---
+++ setDotfiles.sh
@@ -0,0 +1,6 @@
+cp .tmux.conf $HOME/
+cp .emacs $HOME/
+cp -r .emacs.d $HOME/
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git_prompt.sh
+mv git_prompt.sh.1 git_prompt.sh
+cp .zshrc $HOME/
