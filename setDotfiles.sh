68767ee81cd4346de094d728c994893b3cfa2407
ami-GS <1991.daiki@gmail.com>
2016-04-17 06:23:37 +0000

    update

--- setDotfiles.sh
+++ setDotfiles.sh
@@ -3,4 +3,6 @@ cp .emacs $HOME/
 cp -r .emacs.d $HOME/
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git_prompt.sh
 mv git_prompt.sh.1 git_prompt.sh
+sudo apt-get install zsh
+chsh -s /usr/bin/zsh
 cp .zshrc $HOME/
