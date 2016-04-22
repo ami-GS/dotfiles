d495a55e9c1e02c41afe3eb2cd5c99c85d06f337
ami-GS <1991.daiki@gmail.com>
2016-04-22 15:02:38 +0000

    add golang install

--- setDotfiles.sh
+++ setDotfiles.sh
@@ -7,8 +7,11 @@ elif [  "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     if   [  -e /etc/debian_version ] ||
          [  -e /etc/debian_release ]; then
         # Check Ubuntu or Debian
+	# for golang
+	sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable
 	sudo apt-get update
 	sudo apt-get upgrade
+	sudo apt-get install golang
 	sudo apt-get install zsh
     fi
     if type git > /dev/null 2>&1; then
@@ -26,7 +29,6 @@ fi
 cp .tmux.conf $HOME/
 cp .emacs $HOME/
 cp -r .emacs.d $HOME/
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git_prompt.sh
-mv git_prompt.sh.1 git_prompt.sh
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 chsh -s /usr/bin/zsh
 cp .zshrc $HOME/
