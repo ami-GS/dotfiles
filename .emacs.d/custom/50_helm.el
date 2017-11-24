8553a6b0a7d1ca27e0f3acb44a36f291c8e471ca
ami-GS <1991.daiki@gmail.com>
2017-11-24 19:11:41 +0200

    use helm-find-files

--- .emacs.d/custom/50_helm.el
+++ .emacs.d/custom/50_helm.el
@@ -1,3 +1,9 @@
 (require 'helm)
 (helm-mode t)
 (global-set-key (kbd "M-x") 'helm-M-x)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; For find-file etc.
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
