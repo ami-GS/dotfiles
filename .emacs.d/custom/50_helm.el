73aa6153deb2a731bcb438002458bac873bc8f71
ami-GS <1991.daiki@gmail.com>
2017-12-13 18:58:17 +0200

    add helm-gtags

--- .emacs.d/custom/50_helm.el
+++ .emacs.d/custom/50_helm.el
@@ -7,3 +7,14 @@
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 ;; For helm-find-files etc.
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
+(require 'helm-config)
+(require 'helm-gtags)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+;; key bindings
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+              (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+              (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+              (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+              (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
