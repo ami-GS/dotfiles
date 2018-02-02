4a5902227ef5c67018e41d29bda927095b7fcef9
ami-GS <1991.daiki@gmail.com>
2018-02-02 19:11:45 +0900

    fix long left issue (0 -> nil)

--- .emacs.d/custom/20_complete.el
+++ .emacs.d/custom/20_complete.el
@@ -1,5 +1,5 @@
 (when (locate-library "company")
-  (setq company-idle-delay 0) ; デフォルトは0.5
+  (setq company-idle-delay nil) ; デフォルトは0.5
   (setq company-minimum-prefix-length 2) ; デフォルトは4
   (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
   (global-company-mode)
