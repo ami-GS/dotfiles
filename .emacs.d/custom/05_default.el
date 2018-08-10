b232a572295a37e7281f49f1dfc16fc8af52f099
ami-GS <1991.daiki@gmail.com>
2018-08-10 10:22:46 -0700

    enable mouse

--- .emacs.d/custom/05_default.el
+++ .emacs.d/custom/05_default.el
@@ -1,5 +1,5 @@
 
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(xterm-mouse-mode t)
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 
